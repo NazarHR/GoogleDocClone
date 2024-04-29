f1129e182c252b6fddf8b66cabf6a48d07861b83
NazarHR <47197105+NazarHR@users.noreply.github.com>
2024-04-29 09:38:42 +0300

    Initial commit
    created project

---
+++ GoogleDocClone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoogleDocClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

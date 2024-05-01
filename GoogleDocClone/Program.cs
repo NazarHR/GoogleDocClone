8caa41e0a5763f9c77a11dfad167934252620abd
NazarHR <47197105+NazarHR@users.noreply.github.com>
2024-05-01 10:52:16 +0300

    text redactor controller
    added TinyMCE redactor
    added index page with user'sdocuments
    added edit page
    added delite page

--- GoogleDocClone/Program.cs
+++ GoogleDocClone/Program.cs
@@ -43,7 +43,7 @@ namespace GoogleDocClone
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Documents}/{action=Index}");
             app.MapRazorPages();
 
             app.Run();
